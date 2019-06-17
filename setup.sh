67afa1d78c44b85d91e92b6a87285c77a3eb44ee
Matt Wenger <matthewwenger@live.com>
2019-06-17 14:28:23 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,20 @@
+# Sets up 3 containers for apache guacamole
+# This includes guacd, guacamole, and mariadb
+
+# Pull docker images
 docker pull guacamole/guacd
 docker pull guacamole/guacamole
 docker pull mysql
 
+# Run guacamole
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --mysql  initdb.sql
 mkdir /tmp/scripts
 cp initdb.sql /tmp/scripts
 
+# Run guacd container
 docker run --name guac-mysql -v /tmp/scripts:/tmp/scripts -v mysql_volume:/var/lib/mysql -e MYSQL_ROOT_PASSWORD='sqlpassword' -d mysql:latest
 
+# Create the mariaDB database and columns
 mysql -u root -p
  CREATE DATABASE guacamole;
  CREATE USER 'guacamole' IDENTIFIED BY 'sqlpassword';
@@ -15,9 +22,11 @@ mysql -u root -p
  FLUSH PRIVILEGES;
  quit
 
+# enter bash in guacamole container
 docker exec -it guacamole /bin/bash
 cat /tmp/scripts/initdb.sql | mysql -u root -p guacamole;
 
+# Run the final container
 docker run --name guacamole --link guacd:guacd --link guac-mysql:mysql \
 -e MYSQL_DATABASE='guacamole' \
 -e MYSQL_USER='guacamole' \
